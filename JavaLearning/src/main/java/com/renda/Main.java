81533280d60409f665f7d0bd964d94b53caf2db3
张人大 <952402967@qq.com>
2025-04-18 17:40:49 +0800

    Stage1 Week1 Day3 ThreadPool

--- JavaLearning/src/main/java/com/renda/Main.java
+++ JavaLearning/src/main/java/com/renda/Main.java
@@ -1,20 +1,17 @@
 package com.renda;
 
-import com.renda.demo.Person;
-import com.renda.demo.Student;
+import com.renda.demo.thread.RendaRunnable;
 
-import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
     public static void main(String[] args) {
-        Student student = new Student();
-        student.setName("Renda");
-        student.setAge(22);
-        student.setStudentId("123");
-
-        Person person = student;
-        person.printInfo();
-        System.out.println(person);
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+        for (int i = 0; i < 10; i++) {
+            executor.submit(new RendaRunnable(i));
+        }
+        executor.shutdown();
     }
 }
 
