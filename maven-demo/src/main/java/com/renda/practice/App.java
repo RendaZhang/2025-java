a8f9b0291166230e147ea7c626806f13a9d62afa
Renda Zhang <rendazhang@qq.com>
2025-04-22 21:02:20 +0800

    Maven studying

---
+++ maven-demo/src/main/java/com/renda/practice/App.java
@@ -0,0 +1,12 @@
+package com.renda.practice;
+
+import com.google.common.collect.ImmutableList;
+
+public class App {
+
+    public static void main(String[] args) {
+        ImmutableList<String> names = ImmutableList.of("Alice", "Bob", "Charlie");
+        System.out.println("Names: " + names);
+    }
+
+}
