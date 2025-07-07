2162551139aa279536ebdee020ac729239ff93b9
Renda Zhang <rendazhang@qq.com>
2025-07-07 20:56:01 +0800

    Remove redundent file

--- scripts/spotless.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -e
-# Run spotless apply for each Maven module
-for pom in $(git ls-files '*pom.xml'); do
-  dir=$(dirname "$pom")
-  (cd "$dir" && mvn -q com.diffplug.spotless:spotless-maven-plugin:2.44.5:apply)
-done
