63a498db43199c96bddb8b401eb67849a4ae229f
Renda Zhang <rendazhang@qq.com>
2025-09-14 16:40:35 +0800

    LeetCode 215 Kth Largest Element in an Array: Time Limit Exceeded #2 Version

--- leetcode-training/src/main/java/com/renda/leetcode/problems/LC215_KthLargestElementInAnArray.java
+++ leetcode-training/src/main/java/com/renda/leetcode/problems/LC215_KthLargestElementInAnArray.java
@@ -14,23 +14,39 @@ import com.renda.leetcode.core.LeetCodeProblem;
 public class LC215_KthLargestElementInAnArray implements LeetCodeProblem {
 
     public int findKthLargest(int[] nums, int k) {
+        if (nums.length == 1) {
+            return nums[0];
+        }
         LinkedList<Integer> topKLinkedList = new LinkedList<>();
         for (int i = 0; i < k; i++) {
             topKLinkedList.add(Integer.MIN_VALUE);
         }
         for (int i = 0; i < nums.length; i++) {
-            ListIterator<Integer> iterator = topKLinkedList.listIterator();
-            while (iterator.hasNext()) {
-                Integer element = iterator.next();
-                if (nums[i] > element) {
-                    iterator.previous();
-                    iterator.add(nums[i]);
+            ListIterator<Integer> iterator1 = topKLinkedList.listIterator();
+            ListIterator<Integer> iterator2 = topKLinkedList.listIterator(k);
+            if (nums[i] <= topKLinkedList.getLast()) {
+                continue;
+            }
+            while (iterator1.hasNext() && iterator2.hasPrevious()) {
+                Integer element1 = iterator1.next();
+                Integer element2 = iterator2.previous();
+                if (nums[i] <= element2) {
+                    iterator2.next();
+                    iterator2.add(nums[i]);
                     topKLinkedList.removeLast();
                     break;
+                } else {
+                    if (nums[i] >= element1) {
+                        iterator1.previous();
+                        iterator1.add(nums[i]);
+                        topKLinkedList.removeLast();
+                        break;
+                    }
                 }
+                if (iterator1.nextIndex() > iterator2.nextIndex()) break;
             }
         }
-        return topKLinkedList.get(k-1);
+        return topKLinkedList.getLast();
     }
 
     @Override
@@ -43,9 +59,13 @@ public class LC215_KthLargestElementInAnArray implements LeetCodeProblem {
         System.out.println("LC215 Kth Largest Element in an Array: ");
         System.out.println(findKthLargest(new int[]{3,2,1,5,6,4}, 2));
         System.out.println(findKthLargest(new int[]{3,2,3,1,2,4,5,5,6}, 4));
+        System.out.println(findKthLargest(new int[]{1}, 1));
+        System.out.println(findKthLargest(new int[]{2,1}, 2));
         // Expected Output:
         // 5
         // 4
+        // 1
+        // 1
     }
 
 }
